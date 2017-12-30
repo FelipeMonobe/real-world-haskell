53acf549b666473c0f0cf27612ad7d0ce95f4ab9
FelipeMonobe <xinube@gmail.com>
2017-12-30 21:55:54 -0200

    chapter 02 - Types and Functions

---
+++ 02-types-and-functions/exercises/02.hs
@@ -0,0 +1,4 @@
+last :: [a] -> a
+-- This signature tells that this expression receives
+-- a list of some type and outputs a single element
+-- of that same type
---
+++ 02-types-and-functions/exercises/04.hs
@@ -0,0 +1,3 @@
+-- run these from GHCI and after loading 03.hs ONLY
+lastButOne [1,2,3] -- 2
+lastButOne [1] -- *** Exception: 03.hs:(2,1)-(3,33): Non-exhaustive patterns in function lastButOne
