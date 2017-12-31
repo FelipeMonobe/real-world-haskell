c7750050a42dcecf42cc021d4977a67be99487c8
FelipeMonobe <xinube@gmail.com>
2017-12-30 22:16:31 -0200

    c2ex03 done with pattern matching and recursion

--- 02-types-and-functions/exercises/03.hs
+++ 02-types-and-functions/exercises/03.hs
@@ -1,3 +1,4 @@
 lastButOne :: [a] -> a
-lastButOne (x:[_]) = x
+lastButOne [] = error "Operation forbidden on lists with less than 2 elements"
+lastButOne (x:_:[]) = x
 lastButOne (_:xs) = lastButOne xs
